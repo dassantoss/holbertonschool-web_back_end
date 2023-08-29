f24a6492086791ceaae3b05b6a1674ee46c0c16a
Santiago E <santy.e@gmail.com>
2023-08-29 17:56:51 -0600

    Fix async declaration in measure_time method

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -10,7 +10,7 @@ from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measures the average execution time for wait_n(n, max_delay)
     Args:
