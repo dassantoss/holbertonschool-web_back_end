120958097da9dd1e30775767becdab3965660eff
Santiago E <santy.e@gmail.com>
2023-08-29 17:44:43 -0600

    Fix variable typing

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Executes multiple async functions concurrently
     Args:
