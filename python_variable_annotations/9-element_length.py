f72d6d7fee47c6d8a46f1fe63b5e7c4dc6d0773b
Santiago E <santy.e@gmail.com>
2023-08-28 19:09:01 -0600

    Add annotations: element_length(lst) -> List[Tuple[Any, int]].

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic annotations - length"""
+from typing import Tuple, List, Sequence, Iterable
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Calculate the length of elements in an iterable of sequences.
+
+    Args:
+        lst (Iterable[Sequence]): An iterable containing sequences.
+
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples, each containing a sequence and its length.
+    """
+    return [(i, len(i)) for i in lst]
