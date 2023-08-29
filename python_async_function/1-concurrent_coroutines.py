50aac533c86f9ce1de1f7e40b7a5706b3b337de0
Santiago E <santy.e@gmail.com>
2023-08-29 17:39:43 -0600

    Add wait_n for concurrent execution

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module that contains asynchronous routines
+"""
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List:
+    """
+    Executes multiple async functions concurrently
+    Args:
+        n (int): Number of times to execute the function
+        max_delay (int): Maximum delay value
+
+    Returns:
+        List[float]: List of delays
+    """
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    return sorted(delays)
