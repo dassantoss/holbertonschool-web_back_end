721669940ee540433d5ee4ce1d8882be848ec329
Santiago E <santy.e@gmail.com>
2023-08-29 17:52:31 -0600

    Add measure_time function for runtime measurement

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module to measure runtime of asynchronous routines
+"""
+import asyncio
+import time
+from typing import List
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measures the average execution time for wait_n(n, max_delay)
+    Args:
+        n (int): Number of times to execute the function
+        max_delay (int): Maximum delay value
+
+    Returns:
+        float: Average execution time per function call
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
