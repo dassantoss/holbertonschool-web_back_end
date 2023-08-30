e017e30c34b5d7c152a8d935892d5b12c02044dd
Santiago E <santy.e@gmail.com>
2023-08-29 18:10:55 -0600

    Fix: Sort delays in ascending order in task_wait_n

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -20,4 +20,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
-    return delays
+    return sorted(delays)
