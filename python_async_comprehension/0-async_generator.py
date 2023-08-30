3b67ae846962c69553e1b69bbef4b733d68d2816
Santiago E <santy.e@gmail.com>
2023-08-29 19:23:35 -0600

    Fix async generator type annotation and sleep usage

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ Module with an asynchronous generator.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronous generator that yields random numbers between 0 and 10.
 
