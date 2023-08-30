aa4a8629d2634cc1f73b2933b3a840e0522f97b5
Santiago E <santy.e@gmail.com>
2023-08-29 19:39:45 -0600

    Add Measure total runtime of parallel async comprehensions

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""""""
+import asyncio
+from typing import List
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Function that measures the total runtime and returns it.
+
+    Returns:
+        float: The total runtime.
+    """
+    start_time = asyncio.get_event_loop().time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = asyncio.get_event_loop().time()
+    return end_time - start_time
