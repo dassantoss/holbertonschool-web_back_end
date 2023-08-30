3cfbc18d66d0ca7f120094c58a42b797bc1d975f
Santiago E <santy.e@gmail.com>
2023-08-29 18:07:05 -0600

    Add task_wait_n using asyncio.Tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module that contains asynchronous routines using asyncio.Tasks
+"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Executes multiple async functions using asyncio.Tasks
+    Args:
+        n (int): Number of times to execute the function
+        max_delay (int): Maximum delay value
+
+    Returns:
+        List[float]: List of delays
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return delays
