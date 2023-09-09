81bd3ad2500757e868b7fd01c8e7b2c9f7e34994
Santiago E <santy.e@gmail.com>
2023-09-08 19:48:29 -0600

    Add Hypermedia pagination

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""
+Server class for paginating a database of popular baby names.
+"""
+from typing import Tuple
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple containing the start and end index
+    for a given page and page size.
+
+    Args:
+        page (int): The page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start and end index.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get a page of data from the dataset.
+        Args:
+            page (int): The page number (1-indexed).
+            page_size (int): The number of items per page.
+        Returns:
+            List[List]: A list of rows corresponding to the requested page.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
+
+        if start_index >= len(data):
+            return []
+
+        return data[start_index:end_index]
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Get a page of data from the dataset and return
+        hypermedia pagination information.
+        Args:
+            page (int): The page number (1-indexed).
+            page_size (int): The number of items per page.
+        Returns:
+            dict: A dictionary containing the following keys:
+                - page_size: The length of the returned dataset page.
+                - page: The current page number.
+                - data: The dataset page (equivalent to return from previous task).
+                - next_page: The next page number, or None if no next page.
+                - prev_page: The previous page number, or None if no previous page.
+                - total_pages: The total number of pages in the dataset as an integer.
+        """
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
+
+
+if __name__ == "__main__":
+    # Example usage
+    res = index_range(1, 7)
+    print(type(res))
+    print(res)
+
+    res = index_range(page=3, page_size=15)
+    print(type(res))
+    print(res)
