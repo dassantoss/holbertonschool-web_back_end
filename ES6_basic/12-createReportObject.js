8cbfb4fcbbed47bd4d3bcc2e4d4352a88008e58c
Santiago E <santy.e@gmail.com>
2023-09-14 21:26:09 -0600

    Fix

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,16 +1,12 @@
 export default function createReportObject(employeesList) {
-  // Definimos un objeto llamado "allEmployees".
   const allEmployees = {};
 
-  // Recorremos el objeto "employeesList" y asignamos los empleados a sus respectivos departamentos en "allEmployees".
   for (const department in employeesList) {
     allEmployees[department] = employeesList[department];
   }
 
-  // Definimos el método "getNumberOfDepartments" que devuelve la cantidad de departamentos.
   const getNumberOfDepartments = () => Object.keys(allEmployees).length;
 
-  // Devolvemos un objeto con "allEmployees" y "getNumberOfDepartments".
   return {
     allEmployees,
     getNumberOfDepartments,
