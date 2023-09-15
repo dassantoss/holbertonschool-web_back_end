17eaaa9ea44f4c55d77843e039e80131506978a6
Santiago E <santy.e@gmail.com>
2023-09-15 12:28:08 -0600

    Add initializeRooms function that creates ClassRoom objects

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from './0-classroom.js';
+
+function initializeRooms() {
+  const rooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
+  ];
+  return rooms;
+}
+export default initializeRooms;
