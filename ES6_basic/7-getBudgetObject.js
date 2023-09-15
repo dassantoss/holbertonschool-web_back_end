f4200e3950098260b015748c37997809465fe36f
Santiago E <santy.e@gmail.com>
2023-09-14 19:10:27 -0600

    Add object property value shorthand syntax

---
+++ ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+export default function getBudgetObject(income, gdp, capita) {
+  // Utilizamos la sintaxis de atajo de valores de propiedad de objetos aquí.
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
