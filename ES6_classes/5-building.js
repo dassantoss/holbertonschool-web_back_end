08447e52ef0f661a2c8b205e3006d65a607f9f7a
Santiago E <santy.e@gmail.com>
2023-09-15 16:24:20 -0600

    Fix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,8 +1,7 @@
 export default class Building {
   constructor(sqft) {
-    // Verifica si la instancia actual es una clase hija de Building
-    // y si no se ha implementado evacuationWarningMessage
-    if (this.constructor === Building && this.evacuationWarningMessage === undefined) {
+    if (this.constructor !== Building
+      && this.evacuationWarningMessage === undefined) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
