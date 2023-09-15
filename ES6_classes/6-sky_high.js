62859849668fba912e6da2a03b2fd5c8585aca01
Santiago E <santy.e@gmail.com>
2023-09-15 16:30:55 -0600

    Add SkyHighBuilding class

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Llama al constructor de la clase base (Building)
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  // Override del método evacuationWarningMessage
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
