dfcd839ac8640c9c19d31bb0e7fa342731e9d783
Santiago E <santy.e@gmail.com>
2023-09-15 07:51:46 -0600

    Fix

--- ES6_basic/101-iterateThroughObject.js
+++ ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = '';
-  for (const employee of reportWithIterator) {
-    result += `${employee} | `;
-  }
-  // Eliminamos el último '|'
-  return result.slice(0, -2);
+  return Object.split('|');
 }
