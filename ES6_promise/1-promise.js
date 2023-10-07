8ee8004543ac364253be85f7e1c79162f79b08f1
Santiago E <santy.e@gmail.com>
2023-10-07 14:30:57 -0600

    Upgrade: Fix eslint

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,8 @@
 function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: 'Success'});
-    }
-    else {
+      resolve({ status: 200, body: 'Success' });
+    } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
