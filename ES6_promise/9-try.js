1c32db4a48a84de268684827227e60d1d64a3d74
Santiago E <santy.e@gmail.com>
2023-10-08 12:11:51 -0600

    Add Throw an error

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
