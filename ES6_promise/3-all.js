5bfbfde9ce633f85d967ed683611cbb647f96533
Santiago E <santy.e@gmail.com>
2023-10-08 11:12:31 -0600

    Fix

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
